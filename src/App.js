dd41ab2ddc44f57c00d613b136806302d126b551
Ramos Felipe <felipeofor@gmail.com>
2021-12-10 15:24:33 -0300

    require de Json[app.js] y creaciones de funciones.js[utils]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { funciones } from './utils/funciones';
 
 function App() {
+ 
+  //consumiendo del Json
+  const json = require('./response.json');
+  const auditorias = json.auditorias;
+  
+funciones(auditorias);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Segundo Json</h1>
     </div>
   );
 }
---
+++ src/utils/funciones.js
@@ -0,0 +1,3 @@
+export function funciones(parametro) {
+   console.log(parametro);
+}
